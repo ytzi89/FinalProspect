f90b877679c51765eadec73ef8cc74dd713a2718
Skyler Summers <skytzi89@gmail.com>
2014-01-27 20:10:32 -0800

    Added “CameraController.js” script and simple skeleton.

---
+++ FinalProspect/Assets/Scripts/CameraController.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+// Public variables
+
+var cameraTarget: GameObject;
+var offset: float;
+
+// Private variables
+
+function Start () {
+
+}
+
+// Use LateUpdate for camera
+function LateUpdate () {
+
+	// Update camera position
+	transform.position = Vector3(
+							cameraTarget.transform.position.x,
+							cameraTarget.transform.position.y + 5, 
+							cameraTarget.transform.position.z - offset
+							);
+							
+	// Update camera rotation
+	transform.LookAt(Vector3(
+						transform.position.x, 
+						cameraTarget.transform.position.y + cameraTarget.collider.bounds.extents.y, 
+						cameraTarget.transform.position.z);
+
+}
