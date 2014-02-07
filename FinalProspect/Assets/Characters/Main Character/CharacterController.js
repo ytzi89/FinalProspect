d8119f4ae08570cdc5419034e363e1e4494e521a
arsenic0701 <deblecan0701@gmail.com>
2014-02-07 13:25:51 -0800

    added code that allow player to jump when spacbar is press. also set a gravity variable that controls the rate at which the main character falls once in the air

--- FinalProspect/Assets/Characters/Main Character/CharacterController.js
+++ FinalProspect/Assets/Characters/Main Character/CharacterController.js
@@ -8,11 +8,15 @@
 
 private var pHealth: float;
 private var pSpeed: float;
+private var pjumpspeed: float; //control how fast the main character jumps
+private var gravity: float; //set the gravity 
 
 function Start () {
 
 	pHealth = 100;
 	pSpeed = 8;
+	pjumpspeed = 2; //set the speed at which the player jumps 
+	gravity = 5;  //set gravity which controls how fast or slow the main character falls
 
 }
 
@@ -25,11 +29,28 @@ function Update () {
 // Player movement
 function Movement()
 {
-
+	
+	
+	//setting both the horizontal and vertical movement of our character 
 	var horizontalMovement = Input.GetAxis("Horizontal") * pSpeed * Time.deltaTime;
 	var verticalMovement = Input.GetAxis("Vertical") * pSpeed * Time.deltaTime * 0.75;
-	
+	//store the horizont and vertical movement into on 3d vector variable
 	var movement = Vector3(horizontalMovement, 0, verticalMovement);
+	
+		
+	
+		//if the space bar is pressed jump 
+			if(Input.GetButton("Jump"))
+			{
+				movement.y = pjumpspeed;
+				
+			}
+			    //apply the effects of gravity 
+				movement.y -= gravity*Time.deltaTime;
+		
+		
+		
+	
 	
 	transform.position += movement;
 		
