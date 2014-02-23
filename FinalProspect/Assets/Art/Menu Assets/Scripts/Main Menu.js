eb8a903ef77a201a819a3146231c433e35c2ddf1
KVet92 <kevin.vetterly@gmail.com>
2014-02-22 22:53:21 -0500

    Added Main Menu and background music
    I also fixed the lighting in the second level so the ground is properly
    lighted as well.

---
+++ FinalProspect/Assets/Art/Menu Assets/Scripts/Main Menu.js
@@ -0,0 +1,29 @@
+﻿var isQuit=false;
+
+function OnMouseEnter(){
+//change text color
+renderer.material.color=Color.red;
+}
+
+function OnMouseExit(){
+//change text color
+renderer.material.color=Color.blue;
+}
+
+function OnMouseUp(){
+//is this quit
+if (isQuit==true) {
+//quit the game
+Application.Quit();
+}
+else {
+//load level
+Application.LoadLevel("Level1");
+}
+}
+
+function Update(){
+//quit game if escape key is pressed
+if (Input.GetKey(KeyCode.Escape)) { Application.Quit();
+}
+}
